8437edf0b4585d4b273b056720b1e30c0ddb6789
Symro <sylvain.mro@gmail.com>
2013-10-20 18:57:53 +0200

    interaction avec la bdd
    Affichage des markers entré dans la bdd

--- index.php
+++ index.php
@@ -10,27 +10,63 @@
       body { height: 100%; margin: 0; padding: 0 }
       #map-canvas { height: 100% }
     </style>
+    <?php 
+      // Connexion avec la BDD
+      $bdd=new PDO('mysql:host=localhost;dbname=parislack','root','');
+      // Obtenir la liste des points
+      $result=$bdd->query('SELECT latitude,longitude FROM listemarkers ORDER BY id');
+      // On récupère la liste des markers dans un tableau
+      $listMarkers='';
+      while ($row=$result->fetch()) {
+        if ($listMarkers!='')$listMarkers.=','; //separation de la longitude et latitude par une virgule
+        $listMarkers.='['.$row['latitude'].','.$row['longitude'].']';
+      }
+      // Fermeture de la connexion
+      $result->closeCursor();
+     ?>
     <script language="JavaScript" type="text/javascript">
       var map;
       var pos;
+
       function initialize() {
+        // On défini le niveau de zoom, ainsi que la position ou la map sera centrée puis son type
         var mapOptions = {
           zoom: 13,
-          center: new google.maps.LatLng(48.856614, 2.3522219000000177),
+          center: new google.maps.LatLng(48.856614, 2.352221),
           mapTypeId: google.maps.MapTypeId.ROADMAP
         };
-
+        // Initialisation de la map
         map = new google.maps.Map(document.getElementById('map-canvas'),mapOptions);
 
+        // Listener pour l'ajout d'un marker au clic droit
         google.maps.event.addListener(map, 'rightclick',function(event){
           placeMarker(event.latLng,map);
         });
 
+        var list_markers=[ <?php echo $listMarkers; ?> ];
+        var i=0;
+        li=list_markers.length;
+        while(i<li){
+          new google.maps.Marker({
+            position:new google.maps.LatLng(list_markers[i][0],list_markers[i][1]),
+            map:map,
+            title:'Marker '+i
+          });
+          i++;
+        }
+
       }
 
+      // function pour placer un marker
       function placeMarker(location,map){
+        // On demande à l'user si il souhaite bien ajouter un marqueur
         var r=confirm("Êtes vous sur de vouloir ajouter un spot ici ?")
         if (r==true) {
+          // Si true alors on ajoute le marker
+          var lat=location.lat();
+          var lng=location.lng();
+          console.log('latitude du marker : '+lat);
+          console.log('longitude du marker : '+lng);
           var marker=new google.maps.Marker({
             position:location,
             map:map
@@ -39,7 +75,9 @@
         }
       }
 
+      // Function de geolocalisation
       function trouve(){
+          // Si le navigateur autorise la geoloc
           if (navigator.geolocation) {
             navigator.geolocation.getCurrentPosition(function(position){
               // On récupère la position
@@ -47,13 +85,7 @@
               console.log(pos);
               map.setCenter(pos);
               map.setZoom(15);
-              // affichage d'un panneau
-              // var panel=new google.maps.InfoWindow({
-              //   map:map,
-              //   position:pos,
-              //   content:"Vous êtes ici"
-              // });
-              // Affiche un marker
+              // On place un marker sur l'endroit géolocalisé
               pos=new google.maps.Marker({
                 position:pos,
                 id:position,
@@ -61,10 +93,11 @@
                 animation:google.maps.Animation.BOUNCE,
                 title:"Vous êtes ici"
               });
-
             });
           }
-        }
+      }
+
+      
 
     </script>
   </head>
