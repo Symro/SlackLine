42ea7ce72f3bb041028d5c14906398dd896a3c5e
Flo <florent.prod@live.fr>
2013-10-27 18:13:16 +0100

    Ajout de donnée fictive

--- parislack.sql
+++ parislack.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Client: localhost
--- Généré le: Dim 20 Octobre 2013 à 18:07
+-- Généré le: Dim 27 Octobre 2013 à 17:10
 -- Version du serveur: 5.5.8-log
 -- Version de PHP: 5.3.5
 
@@ -17,7 +17,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8 */;
 
 --
--- Base de données: `slackline`
+-- Base de données: `hetic_slackline`
 --
 
 -- --------------------------------------------------------
@@ -29,8 +29,8 @@ SET time_zone = "+00:00";
 CREATE TABLE IF NOT EXISTS `spots` (
   `id` int(5) NOT NULL AUTO_INCREMENT,
   `id_utilisateur` int(5) NOT NULL,
-  `latitude` decimal(10,0) NOT NULL,
-  `longitude` decimal(10,0) NOT NULL,
+  `latitude` decimal(10,6) NOT NULL,
+  `longitude` decimal(10,6) NOT NULL,
   `titre` varchar(70) NOT NULL,
   `description` varchar(255) NOT NULL,
   `adresse` varchar(150) NOT NULL,
@@ -39,7 +39,17 @@ CREATE TABLE IF NOT EXISTS `spots` (
   `categorie` varchar(70) NOT NULL,
   `date_creation` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=5 ;
+
+--
+-- Contenu de la table `spots`
+--
+
+INSERT INTO `spots` (`id`, `id_utilisateur`, `latitude`, `longitude`, `titre`, `description`, `adresse`, `materiel`, `note`, `categorie`, `date_creation`) VALUES
+(1, 6, '23.516500', '42.986099', 'Mon premier spot de Slackline', 'description du spot sur 255 caractères', 'Ourcq', 'j''emmène une 20m', 4, 'shortline', '2013-10-27 10:14:39'),
+(2, 6, '21.416519', '42.486145', 'Mon deuxième spot de Slackline', 'description du 2ème spot', 'Canal de l''Ourcq', 'description du matériel', 5, 'longline', '2013-10-27 10:15:56'),
+(3, 7, '11.594940', '13.184849', 'Mon troisième spot', 'description du 3ème spot', 'Chevaleret', 'description du matériel', 1, 'shortline', '2013-10-27 14:45:08'),
+(4, 8, '42.516891', '46.594946', 'Mon quatrième spot', 'description du 4ème spot', '', '', 4, 'shortline', '2013-10-27 17:10:17');
 
 -- --------------------------------------------------------
 
@@ -53,7 +63,16 @@ CREATE TABLE IF NOT EXISTS `spots_favoris` (
   `id_spot` int(5) NOT NULL,
   `note` tinyint(1) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=5 ;
+
+--
+-- Contenu de la table `spots_favoris`
+--
+
+INSERT INTO `spots_favoris` (`id`, `id_utilisateur`, `id_spot`, `note`) VALUES
+(1, 6, 1, 4),
+(2, 6, 2, 2),
+(3, 8, 3, 5);
 
 -- --------------------------------------------------------
 
@@ -89,7 +108,50 @@ CREATE TABLE IF NOT EXISTS `utilisateurs` (
   `materiel` tinyint(1) NOT NULL,
   `telephone` varchar(10) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=12 ;
+
+--
+-- Contenu de la table `utilisateurs`
+--
+
+INSERT INTO `utilisateurs` (`id`, `nom`, `prenom`, `email`, `date_naissance`, `mdp`, `niveau`, `technique`, `description`, `materiel`, `telephone`) VALUES
+(5, 'Utilisateur 6', 'Prénom utilisateur 6', 'user6@lol.fr', '1991-05-24', '8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92', 'debutant', 'shortline, trickline', 'Ma petite description\r\n', 0, '0123456789'),
+(6, 'Utilisateur 4', 'Prénom utilisateur 4', 'user4@lol.fr', '2013-10-09', '8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92', 'intermediaire', 'shortline, trickline', 'Ma description', 0, '0123456849'),
+(7, 'Utilisateur 5', 'Prénom utilisateur 5', 'user5@lol.fr', '2013-10-16', '8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92', 'intermediaire', 'shortline, trickline, jumpline', 'Ma description', 0, '0123456849'),
+(8, 'Utilisateur 1', 'Prénom Utilisateur 1', 'user1@lol.fr', '2013-10-02', '8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92', 'debutant', 'shortline', 'Ma description', 1, '0633972522'),
+(9, 'Utilisateur 2', 'Prénom Utilisateur 2', 'user2@lol.fr', '2013-11-02', '8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92', 'intermediaire', 'longline', 'Ma description', 1, '0666666666'),
+(10, 'Utilisateur 3', 'Prénom Utilisateur 3', 'user3@lol.fr', '2013-02-02', '8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92', 'intermediaire', 'highline', 'Ma description', 0, '0198765432');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `utilisateurs_favoris`
+--
+
+CREATE TABLE IF NOT EXISTS `utilisateurs_favoris` (
+  `id` int(5) NOT NULL AUTO_INCREMENT,
+  `id_utilisateur` int(5) NOT NULL,
+  `id_favoris` int(5) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=14 ;
+
+--
+-- Contenu de la table `utilisateurs_favoris`
+--
+
+INSERT INTO `utilisateurs_favoris` (`id`, `id_utilisateur`, `id_favoris`) VALUES
+(1, 6, 5),
+(2, 6, 7),
+(3, 7, 6),
+(4, 5, 7),
+(5, 5, 8),
+(6, 5, 9),
+(7, 5, 10),
+(8, 6, 6),
+(9, 6, 7),
+(10, 6, 8),
+(12, 6, 6),
+(13, 6, 6);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
