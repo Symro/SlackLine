72f47970894b218359d64208f61e2861e7c5e384
Alexandre Guerard <alexandre.guerard6@gmail.com>
2013-11-28 20:59:56 +0100

    Ajout des icones + Integration inscription.php

--- index.php
+++ index.php
@@ -31,25 +31,23 @@ include('form_login.php');
               <input type="password" id="password" name="password" placeholder="PASSWORD" required>
               <input type="submit" id="login" name="login" value="OK">
             </form>
-              <section class="section-right slideLeft">
-                <div class="iphone hidden-phone"></div>
-              </section>      
-              </br></br>
-              <p>Ou se connecter avec:</p>
-              <a href="#" id="fb-connect">Completer avec Facebook</a>
-              <a href="#" id="google-connect">Completer avec Google+</a>
-              <hr></form>
-              <a href="inscription.php" class="btn-slack" >INSCRIS-TOI</a>
+            <section class="section-right slideLeft">
+              <div class="iphone hidden-phone"></div>
+            </section>      
+            </br>
+            <p>Ou se connecter avec:</p>
+            <a href="#" id="fb-connect">Completer avec Facebook</a>
+            <a href="#" id="google-connect">Completer avec Google+</a>
+            <hr></form>
+            <button class="btn-slack"><a href="inscription.php">INSCRIS-TOI</a></button>
           </div>
         </div>
       </div>
     </div>
   </div>
-    <a class="arrows-down" href="#secondPage" title="Découvrez la slackline"></a>
+  <a class="arrows-down" href="#secondPage" title="Découvrez la slackline"></a>
 </div>
 <!-- FIN Section0-->
-
-
 
 <!-- Section1: Présentation de la SlackLine  -->
 <div class="section" id="section1">
@@ -102,73 +100,83 @@ include('form_login.php');
     </div>
   </div>
 </div>
+<!-- FIN Section1-->
 
-
-
-
-
+<!-- Section2: Présentation des différentes pratique de la Slack-->
 <div class="section" id="section2">
   <div class="container ">      
     <div class="row">
       <div id="owl-section2" class="owl-carousel">
-          <div class="item">
-            <div class="">
-              <img src="img/anim/anim1.png" alt="HighLine">
-            </div>
-            <div class="tableau">
-              <h1>HIGHLINE</h1>
-              <p>La highline (haute corde) nécessite une sangle tendue au-dessus de 5 mètres de hauteur. À une telle hauteur et a fortiori au delà, un équipement de sécurité doit être mis en place afin de ne pas risquer sa vie.</p>
-            </div>
-          </div>      
-          <div class="item">
-            <div class="">
-              <img src="img/anim/anim1.png" alt="The Last of us">
-             </div>
-            <div class="tableau">
-              <h1>Trickline</h1>
-              <p>La highline (haute corde) nécessite une sangle tendue au-dessus de 5 mètres de hauteur. À une telle hauteur et a fortiori au delà, un équipement de sécurité doit être mis en place afin de ne pas risquer sa vie.</p>
-            </div>
+        <div class="item">
+          <div class="">
+            <img src="img/icons/shortline.svg" alt="HighLine">
           </div>
-          <div class="item">
-            <div class="">
-              <img src="img/anim/anim1.png" alt="The Last of us">
-            </div>
-            <div class="tableau">
-              <h1>Trickline</h1>
-              <p>La highline (haute corde) nécessite une sangle tendue au-dessus de 5 mètres de hauteur. À une telle hauteur et a fortiori au delà, un équipement de sécurité doit être mis en place afin de ne pas risquer sa vie.</p>
-            </div>
+          <div class="tableau">
+            <h1>SHORTLINE</h1>
+            <p>La highline (haute corde) nécessite une sangle tendue au-dessus de 5 mètres de hauteur. À une telle hauteur et a fortiori au delà, un équipement de sécurité doit être mis en place afin de ne pas risquer sa vie.</p>
           </div>
-          <div class="item">
-            <div class="">
-              <img src="img/anim/anim1.png" alt="The Last of us">
+        </div>      
+        <div class="item">
+          <div class="">
+            <img src="img/icons/longline.svg" alt="The Last of us">
             </div>
-            <div class="tableau">
-              <h1>Trickline</h1>
-              <p>La highline (haute corde) nécessite une sangle tendue au-dessus de 5 mètres de hauteur. À une telle hauteur et a fortiori au delà, un équipement de sécurité doit être mis en place afin de ne pas risquer sa vie.</p>
-            </div>
+          <div class="tableau">
+            <h1>LONGLINE</h1>
+            <p>La highline (haute corde) nécessite une sangle tendue au-dessus de 5 mètres de hauteur. À une telle hauteur et a fortiori au delà, un équipement de sécurité doit être mis en place afin de ne pas risquer sa vie.</p>
           </div>
-          <div class="item">
-            <div class="">
-              <img src="img/anim/anim1.png" alt="The Last of us">
-            </div>
-            <div class="tableau">
-              <h1>Trickline</h1>
-              <p>La highline (haute corde) nécessite une sangle tendue au-dessus de 5 mètres de hauteur. À une telle hauteur et a fortiori au delà, un équipement de sécurité doit être mis en place afin de ne pas risquer sa vie.</p>
-            </div>
+        </div>
+        <div class="item">
+          <div class="">
+            <img src="img/icons/trickline.svg" alt="The Last of us">
           </div>
-          <div class="item">
-            <div class="">
-              <img src="img/anim/anim1.png" alt="The Last of us">
-            </div>
-            <div class="tableau">
-              <h1>Trickline</h1>
-              <p>La highline (haute corde) nécessite une sangle tendue au-dessus de 5 mètres de hauteur. À une telle hauteur et a fortiori au delà, un équipement de sécurité doit être mis en place afin de ne pas risquer sa vie.</p>
-            </div>
+          <div class="tableau">
+            <h1>TRICKLINE</h1>
+            <p>La highline (haute corde) nécessite une sangle tendue au-dessus de 5 mètres de hauteur. À une telle hauteur et a fortiori au delà, un équipement de sécurité doit être mis en place afin de ne pas risquer sa vie.</p>
           </div>
-        </div>                
+        </div>
+        <div class="item">
+          <div class="">
+            <img src="img/icons/jumpline.svg" alt="The Last of us">
+          </div>
+          <div class="tableau">
+            <h1>JUMPLINE</h1>
+            <p>La highline (haute corde) nécessite une sangle tendue au-dessus de 5 mètres de hauteur. À une telle hauteur et a fortiori au delà, un équipement de sécurité doit être mis en place afin de ne pas risquer sa vie.</p>
+          </div>
+        </div>
+        <div class="item">
+          <div class="">
+            <img src="img/icons/highline.svg" alt="The Last of us">
+          </div>
+          <div class="tableau">
+            <h1>HIGHLINE</h1>
+            <p>La highline (haute corde) nécessite une sangle tendue au-dessus de 5 mètres de hauteur. À une telle hauteur et a fortiori au delà, un équipement de sécurité doit être mis en place afin de ne pas risquer sa vie.</p>
+          </div>
+        </div>
+        <div class="item">
+          <div class="">
+            <img src="img/icons/waterline.svg" alt="The Last of us">
+          </div>
+          <div class="tableau">
+            <h1>WATERLINE</h1>
+            <p>La highline (haute corde) nécessite une sangle tendue au-dessus de 5 mètres de hauteur. À une telle hauteur et a fortiori au delà, un équipement de sécurité doit être mis en place afin de ne pas risquer sa vie.</p>
+          </div>
+        </div>
+        <div class="item">
+          <div class="">
+            <img src="img/icons/blindline.svg" alt="The Last of us">
+          </div>
+          <div class="tableau">
+            <h1>BLINDLINE</h1>
+            <p>La highline (haute corde) nécessite une sangle tendue au-dessus de 5 mètres de hauteur. À une telle hauteur et a fortiori au delà, un équipement de sécurité doit être mis en place afin de ne pas risquer sa vie.</p>
+          </div>
+        </div>
+      </div>                
     </div>
   </div>
 </div>
+<!-- FIN Section2-->
+
+<!-- Section3: Video de présentation de la Slackline-->
 <div class="section" id="section4">
   <div class="container">
     <div class="row">
@@ -179,36 +187,36 @@ include('form_login.php');
     <span></span>
     <span></span>
   </div>
-  
-  <!-- <a href="" id="controls"></a> -->
-  <video id="video">
-    <source src="assets/ParislackHD.mp4" type='video/mp4' >
+
+  <video id="video" autoplay="false">
+    <source src="assets/pariSlack.mp4" type='video/mp4' >
     <p>Your user agent does not support the HTML5 Video element.</p>
   </video>
-  
+
   <div id="progressBar">
     <span class="progress"></span>
     <span class="buffer"></span>
   </div>
+
   <footer class="hidden-desktop">
-  <div class="container">
-    <div class="row">
-      <ul>
-        <li><a href="#">Mentions légales</a></li>
-        <li><a href="#">Credits</a></li>
-        <li><a href="#">Copyright 2013 - PariSlack</a></li>
-      </ul>
+    <div class="container">
+      <div class="row">
+        <ul>
+          <li><a href="#">Mentions légales</a></li>
+          <li><a href="#">Credits</a></li>
+          <li><a href="#">Copyright 2013 - PariSlack</a></li>
+        </ul>
+      </div>
     </div>
-  </div>
-  
   </footer>
 </div>
+<!-- FIN Section3-->
 
+<!-- Section4: Témoignages-->
 <div class="section hidden-phone" id="section3">
   <div class="container">
     <div class="row">
       <h1><strong>CE QU'ILS </strong>EN PENSENT</h1></br></br></br>
-
      <!--  blockquote -->
       <div class="span4">
         <img class="img-circle" src="img/perso1.png">
@@ -230,7 +238,8 @@ include('form_login.php');
           <blockquote>
           J’apprécie le fait de rester facilement en contact avec les slackers et de pouvoir se retrouver et s’aider.
         </blockquote>
-      </div><!-- /.span4 -->
+      </div>
+      <!-- Fin Blockquote -->
     </div>
   </div>
 
@@ -248,6 +257,6 @@ include('form_login.php');
   </footer>
 
 </div>
-
+<!-- FIN Section4-->
 
 <?php include('footer.php'); ?>
